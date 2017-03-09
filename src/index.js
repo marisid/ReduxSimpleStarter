dafe30aeffcfaf6991068bd48f4c9b8096811923
marisid <sid_marina@hotmail.com>
2017-03-09 17:29:52 +0000

    Remove files to start from scratch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAUc1KgnryFWxLDU_7MQad7iHxXo6bGn7Y';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component and this component
+// will create some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div> //this is JSX
+  );
+}
+
+// Take this component's generated HTML and put
+// it on the page (in the DOM) inside the container element
+ReactDOM.render(<App />, document.querySelector('.container'));
